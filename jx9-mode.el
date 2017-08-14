825930337cd34b84046b498171a428473532d00a
Baldmaster <sergey.skupoy@gmail.com>
2017-08-14 08:32:05 +0300

    Predefined constants, function declaration/call highlihghting

--- jx9-mode.el
+++ jx9-mode.el
@@ -20,14 +20,25 @@
 ;;
 ;;; Code:
 
+;; Indentation level
+(defcustom jx9-tab-width 2
+  "Number of spaces to indent nested statements."
+  :group 'jx9
+  :safe 'integerp
+  :type 'integer)
+
 
 ;; Custom faces
-
 
 (defface jx9-nowdoc-face
   '((t :foreground "medium orchid"
        :weight bold
        ))
+  "Face for nowdoc strings."
+  :group 'jx9-mode )
+
+(defface jx9-function-call-face
+  '((t :foreground "cyan"))
   "Face for nowdoc strings."
   :group 'jx9-mode )
 
@@ -68,6 +79,70 @@
      "return"
      "goto") t))
 
+(defconst jx9-builtin-consts-re
+  (regexp-opt
+   '(
+     "JX9_VERSION"
+     "JX9_ENGINE"
+     "__JX9__"
+     "JX9_OS"
+     "__OS__"
+     "JX9_INT_MAX"
+     "JX9_EOL"
+     "MAXINT"
+     "JX9_INT_SIZE"
+     "PATH_SEPARATOR"
+     "DIRECTORY_SEPARATOR"
+     "DIR_SEP"
+     "__TIME__"
+     "__DATE__"
+     "__FILE__"
+     "__DIR__"
+     "COUNT_NORMAL"
+     "COUNT_RECURSIVE"
+     "SORT_ASC"
+     "SORT_DESC"
+     "SORT_REGULAR"
+     "SORT_NUMERIC"
+     "SORT_STRING"
+     "JX9_URL_SCHEME"
+     "JX9_URL_HOST"
+     "JX9_URL_PORT"
+     "JX9_URL_USER"
+     "JX9_URL_PASS"
+     "JX9_URL_PATH"
+     "JX9_URL_QUERY"
+     "JX9_URL_FRAGMENT"
+     "JX9_QUERY_RFC1738"
+     "JX9_QUERY_RFC3986"
+     "SEEK_SET"
+     "SEEK_CUR"
+     "SEEK_END"
+     "LOCK_EX"
+     "LOCK_SH"
+     "LOCK_NB"
+     "LOCK_UN"
+     "STDIN"
+     "stdin"
+     "STDOUT"
+     "stdout"
+     "STDERR"
+     "stderr"
+     "SCANDIR_SORT_ASC"
+     "SCANDIR_SORT_DESC"
+     "SCANDIR_SORT_NONE"
+     "GLOB_MARK"
+     "GLOB_NOSORT"
+     "GLOB_NOCHECK"
+     "GLOB_NOESCAPE"
+     "GLOB_BRACE"
+     "GLOB_ONLYDIR"
+     "GLOB_ERR"
+     "FILE_USE_INCL_PATH"
+     "FILE_IGN_NL"
+     "FILE_SKIP_EL"
+     "FILE_APPEND") t))
+
 (defconst jx9-identifier-re
   "[^[:digit:]][\\([:alnum:]|_\\)]*")
 
@@ -98,12 +173,29 @@
   (concat
    jx9-nowdoc-start-re "\\(.\\|\n\\)*?\n\\1;"))
 
+(defconst jx9-function-declaration-re
+  (concat
+   "\\s-*function\\("
+   jx9-identifier-re
+   "\\)\\s-*("))
+
+(defconst jx9-function-call-re
+  (concat
+   "\\s-*\\("
+   jx9-identifier-re
+   "\\)\\s-*(.*)"))
+
+;; Font lock keywords
 (setq jx9-font-lock-keywords
       `((,jx9-nowdoc-re . 'jx9-nowdoc-face)
         (,jx9-keywords-re . font-lock-keyword-face)
+        (,jx9-builtin-consts-re . font-lock-keyword-face)
+        (,jx9-function-declaration-re (1 font-lock-string-face))
+        (,jx9-function-call-re (1 'jx9-function-call-face))
         (,jx9-variable-assignment (2 font-lock-variable-name-face))
         (,jx9-for-variable-re  (1 font-lock-variable-name-face))))
 
+;; Syntax table
 (defvar jx9-mode-syntax-table
   (let ((table (make-syntax-table)))
     (modify-syntax-entry ?/ ". 124b" table)
@@ -125,6 +217,7 @@
         (setq font-lock-end (point)))
       (setq font-lock-beg found))))
 
+;; Mode
 (defun jx9-mode ()
   "Major mode for editing Jx9"
   (interactive)
